2e78b96907b55c83ce581718c456fa6de21af137
Lucas Teles <teleslucas@ymail.com>
2025-12-01 10:06:39 -0300

    chore: tests end to end

--- backend/tests/e2e.test.js
+++ backend/tests/e2e.test.js
@@ -3,35 +3,72 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:3000';
 
 describe('Testes E2E da API', () => {
-  it('GET / deve retornar mensagem de boas-vindas', async () => {
-    const response = await axios.get(`${BASE_URL}/`);
-    expect(response.status).toBe(200);
-    expect(response.data).toBe('API ok');
-  });
+    let documentId;
+    const documentData = {
+        document: "14335673000",
+        type: "CPF"
+    };
+    it('GET /inexistente should return 404 status', async () => {
+        try {
+            await axios.get(`${BASE_URL}/inexistente`);
+        } catch (error) {
+            expect(error.response.status).toBe(404);
+        }
+    });
 
-  it('GET /inexistente deve retornar 404', async () => {
-    try {
-      await axios.get(`${BASE_URL}/inexistente`);
-    } catch (error) {
-      expect(error.response.status).toBe(404);
-    }
-  });
-
-    it('/POST should create a new document successfully', async () => {
-        let response = await axios.post(`${BASE_URL}/documents`, {
-            "document": "14335673000",
-            "type": "CPF"
-        });
+    it('POST /documents should create a new document successfully', async () => {
+        let response = await axios.post(`${BASE_URL}/documents`, documentData);
         expect(response.status).toBe(201);
         expect(response.data).toHaveProperty('id');
         expect(response.data).toHaveProperty('document');
         expect(response.data).toHaveProperty('type');
+        documentId = response.data.id;
     });
+
+    it('POST /documents should return error when creating a duplicate document', async () => {
+        try {
+            let response = await axios.post(`${BASE_URL}/documents`, documentData);
+            fail('Deveria ter lançado um erro 409');
+        } catch (error) {
+            expect(error.response.status).toBe(409);
+            expect(error.response.data).toHaveProperty('error', 'Registro em duplicidade');
+        }
+    });
+
+    it('GET /documents should return list of documents', async () => {
+        const response = await axios.get(`${BASE_URL}/documents`);
+        expect(response.status).toBe(200);
+        expect(Array.isArray(response.data)).toBe(true);
+    });
+
+    it('GET /documents/:id should return a specific document', async () => {
+        response = await axios.get(`${BASE_URL}/documents/${documentId}`);
+        expect(response.status).toBe(200);
+        expect(response.data).toHaveProperty('id', documentId);
+        expect(response.data).toHaveProperty('type', documentData.type);
+        expect(response.data).toHaveProperty('document', documentData.document);
+        expect(response.data).toHaveProperty('blocklisted', false);
+        expect(response.data).toHaveProperty('createdAt');
+        expect(response.data).toHaveProperty('updatedAt');
+    });
+
+    it('PATCH /documents/:id should update an existing document', async () => {
+        response = await axios.patch(`${BASE_URL}/documents/${documentId}`, { blocklisted: true });
+        expect(response.status).toBe(200);
+        expect(response.data).toHaveProperty('blocklisted', true);
+    });
+
+    it('DELETE /documents/:id should delete an existing document', async () => {
+        response = await axios.delete(`${BASE_URL}/documents/${documentId}`);
+        expect(response.status).toBe(204);
+    });
+
     it('GET /status deve retornar status da API', async () => {
         const response = await axios.get(`${BASE_URL}/status`);
         expect(response.status).toBe(200);
-        expect(response.data).toHaveProperty('status', 'ok');
         expect(response.data).toHaveProperty('uptime');
-        console.log(response.data)
+        expect(response.data).toHaveProperty('totalRequests');
+        expect(typeof response.data.totalRequests).toBe('number');
     });
+
 });
